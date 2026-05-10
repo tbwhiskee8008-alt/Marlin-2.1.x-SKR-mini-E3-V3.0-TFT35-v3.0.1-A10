b08546b4e9913e4b231074499bc1fa43bec58be5
stephen <tbwhiskee8008@gmail.com>
2026-05-10 13:09:15 -0700

    Fix comment formatting in Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,4 +1,4 @@
-/**
+#define DEFAULT_AXIS_STEPS_PER_UNIT {80, 80, 400, 400-415}/**
  * Marlin 3D Printer Firmware
  * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
